dfbbb357d6321c02e1685304d44af289f7bf6a60
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-06-22 22:06:59 -0400

    column check

--- README.md
+++ README.md
@@ -15,3 +15,18 @@
 - Active player
 - How many tokens do each of the players have left
 - Color of the token that player was assigned
+## Logic
+- we will use a gameState object to keep track of the board and the players
+- First steps: 
+- Start Button: will display a board 
+- Player 1 is now the active player and he has a red token visible
+- He will choose a column.
+- We will check if the column has any empty spaces. If it does we will fill the closest space.
+- We will now check for 4 in a row. 
+- If there is no 4 in a row we will then switch the active player and a yellow token will be visible on the screen.
+- Player 2 will now choose a column. We will check if there is an empy space if there is not then nothing will happen if there is then game over alert will be intitiated. 
+
+
+# The hard part
+- The hard part will be to create a board in JS and make it represent the HTML board that is being displayed on the screen. 
+- 
--- app.js
+++ app.js
@@ -1,14 +1,51 @@
-const board = document.querySelector('#board');
+const boardDisplay = document.querySelector('#board');
 const column = document.querySelector('.col')
 const space = document.querySelector('.space');
 
-for (let i = 0; i < 7; i++) {
-    let currentCol = board.children[i];
-    // currentCol.style.backgroundColor = 'yellow';
-    for (let i = 0; i < 6; i++) {
-    currentSpace = currentCol.children[i];
-    
-    
-    currentSpace.style.backgroundColor = 'orange';
-    }
+
+const board = [];
+
+const gameState = {
+    board: board,
+    players: ['red', 'yellow']
 }
+
+//EVENT LISTENERS
+boardDisplay.addEventListener('click', columnCheck);
+//Check to see if there is an empty spot in column
+//If there is return true else return false
+function columnCheck(event) {
+   let columnOpen = false;
+   let currentCol = event.target.closest('.col');
+   for (let i = 0; i < 6; i++) {
+        
+        console.log(currentCol.children[i]);
+        if (currentCol.children[i].classList.contains('open')) {
+            columnOpen = true;
+        }
+    }
+    console.log(columnOpen);
+    return columnOpen;
+}
+
+function dropToken(event) {
+
+}
+
+for (let i = 0; i < 7; i++) {
+    board.push([])
+}
+console.log(board);
+
+// for (let i = 0; i < 7; i++) {
+//     let currentCol = boardDisplay.children[i];
+    
+//     for (let i = 0; i < 6; i++) {
+//         currentSpace = currentCol.children[i];
+//         currentSpot = currentSpace.children[0];
+//         if (currentSpace.classList.contains('occupied')) {
+//             currentSpot.style.backgroundColor = Player.tokenColor;
+//         }
+//     }
+// }
+
