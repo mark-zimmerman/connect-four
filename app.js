7147fae96c3ccded941bebdcaef73998c05cc419
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-06-23 00:04:11 -0400

    drop function

--- app.js
+++ app.js
@@ -7,29 +7,32 @@ const board = [];
 
 const gameState = {
     board: board,
-    players: ['red', 'yellow']
+    players: ['red', 'yellow'],
 }
 
 //EVENT LISTENERS
 boardDisplay.addEventListener('click', columnCheck);
+
+
 //Check to see if there is an empty spot in column
-//If there is return true else return false
+//If there is call the drop token function and pass in first
 function columnCheck(event) {
    let columnOpen = false;
    let currentCol = event.target.closest('.col');
-   for (let i = 0; i < 6; i++) {
-        
-        console.log(currentCol.children[i]);
+   for (let i = 5; i >= 0; i--) {
         if (currentCol.children[i].classList.contains('open')) {
             columnOpen = true;
+            dropToken(currentCol.children[i]);
+            return;
         }
     }
     console.log(columnOpen);
     return columnOpen;
 }
-
-function dropToken(event) {
-
+//
+function dropToken(dropSpace) {
+    dropSpace.children[0].style.backgroundColor = 'red';
+    dropSpace.classList.remove('open');
 }
 
 for (let i = 0; i < 7; i++) {
