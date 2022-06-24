d05efca7601f1b8b3d5a7923fa7f5d496284973b
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-06-24 11:57:08 -0400

    vertical and forward diagonal check for win

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function columnCheck(event) {
 }
 //Need to push the color to the board array
 //We will pass the columnIndex and row index
-function dropToken(dropSpace, rowIndex, columnIndex) {
+function dropToken(dropSpace) {
     if (gameState.move % 2 !== 0) {
         dropSpace.children[0].style.backgroundColor = 'red';
         gameState.playerTurn = 'red';
@@ -58,41 +58,82 @@ function dropToken(dropSpace, rowIndex, columnIndex) {
 }
 //create a for loop
 function addToBoardObj(rowIndex, currentCol) {
-    console.log(rowIndex);
-    console.log(currentCol);
+    
     for (let i = 0; i < 7; i++) {
         if (boardDisplay.children[i] === currentCol) {
-            console.log(i);
             board[i].push(gameState.playerTurn);
-            console.log(board)
         }
     }
     checkForWin(rowIndex, currentCol);
-    console.log(gameState.playerTurn)
 }
 
 function checkForWin(row, column) {
-    let testCol;
-    let testRow; 
-    let vertMatchCount = 1;
-    let horMatchCount = 1;
-    let forDiagMatchCount = 1;
-    let backDiagMatchCount = 1;
-    console.log('check for win')
-    //Up direction
-    for (let i = 0; i < 4; i++) {
-        testRow = row + 1;
-        if(gameState.board[column][testRow] === gameState.board[column][row]) {
-            console.log('hi')
-            vertMatchCount++;
-            console.log(vertMatchCount)
+    let colIndex;
+    
+    for (let i = 0; i < 7; i++) {
+        if (boardDisplay.children[i] === column) {
+            colIndex = i;
         }
+    }
+    checkVertical(row, colIndex);
+    checkForwardDiagonal(row, colIndex);
+
+}
+
+//We have the coordinates of the current token
+//we need to loop through board column to see if the space is a match
+//If it is a match add 1 to vmcount and continue looping
+//if it is not a match exit loop
+//Now we do the same loop for down
+//once it fails we will see what the vertMatchCount is
+//If its 4 we call the win function
+function checkVertical(row, colIndex) {
+    let vertMatchCount = 0;
+    for (let i = 0; i < 4; i++) {
+        let testRow = row - i;
+        if (board[colIndex][row] === board[colIndex][testRow]) {
+            vertMatchCount++;
+        } else {
+            return;
+        }
+    }
+    if (vertMatchCount === 4) {
+        console.log('you win downward!!!');
     }
 }
 
-
-
-
-
+//For forward diagonal
+//we have the coordinates of the current space
+// we need to loop through 4 times
+//each time adding 1 to the colIndex and one to the row
+// So we have the upward direction match count
+// Now we need to count the downward count
+function checkForwardDiagonal(row, colIndex) {
+    let fDMatchCount = 0;
+    let backMatchCount = 0;
+    for (let i = 0; i < 4; i++) {
+        let testCol = colIndex + i;
+        let testRow = row + i;
+        if (board[colIndex][row] === board[testCol][testRow]) {
+            fDMatchCount++; 
+        } else {
+            break;
+        }
+    }
+    for (let i = 0; i < 4; i++) {
+        let testCol = colIndex - i;
+        let testRow = row - i;
+        if (board[colIndex][row] === board[testCol][testRow]) {
+            backMatchCount++; 
+        } else {
+            break;
+        }
+        console.log(backMatchCount);
+    }
+    
+    if (backMatchCount + fDMatchCount > 4) {
+        console.log('You win Diagonal')
+    }
+}
 
 
